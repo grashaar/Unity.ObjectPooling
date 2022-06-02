d7cfbc00cbdbff9cbecfc3da95dcaf840d96732c
datndwolffun <datnd@wolffungame.com>
2022-06-02 22:30:18 +0700

    Change abstract AsyncInstantiator to interface IAsyncInstantiator

---
+++ Unity.ObjectPooling/UnityEngine/IAsyncInstantiator.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_OBJECTPOOLING_UNITASK
+using Cysharp.Threading.Tasks;
+#else
+using System.Threading.Tasks;
+#endif
+
+namespace UnityEngine
+{
+    public interface IAsyncInstantiator<T>
+    {
+#if UNITY_OBJECTPOOLING_UNITASK
+        UniTask<T> InstantiateAsync();
+#else
+        Task<T> InstantiateAsync();
+#endif
+    }
+}
